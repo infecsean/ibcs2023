68a49d11b80e7ed7fa1394437a5c5c01ad5e2fd2
infecsean <10450105a@gmail.com>
2021-09-14 13:19:53 +0800

    my lunch break gone im still on the SECOND QUESTION AHHHHH

--- intro/variables/lists_exercises.py
+++ intro/variables/lists_exercises.py
@@ -3,13 +3,18 @@
 # 01. sum all of the elements in the list
 mylist = [1, 2, 3, 4, 5]
 sum = sum(mylist)
-print(sum)
+# print(sum)
 
 # 02. write a program that removes all duplicates from a list
 mylist = [1, 1, 1, 2, 2, 3, 4, 4]
+unduped = []
 
-unduped = [x for x in mylist if x != x + 1]
-print(unduped)
+x = 0
+while mylist[x] == mylist[x + 1]:
+    mylist.remove(x)
+    x = x + 1
+# unduped = [mylist.remove(x) for x in mylist if x == x + 1]
+print(mylist)
 
 # 03. Write a program that finds the intersection of two lists
 # 04. Write a program that finds the union of two lists, omitting duplicates
