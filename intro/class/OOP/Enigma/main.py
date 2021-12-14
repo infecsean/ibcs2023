fbbdcf5fd5fc45b1ee4b9c9dfb5f6884a69db558
infecsean <10450105a@gmail.com>
2021-12-14 09:42:55 +0800

    bro

--- intro/class/OOP/Enigma/main.py
+++ intro/class/OOP/Enigma/main.py
@@ -1,7 +1,48 @@
 # main.py
-import numpy.random as random
+import os
+from Enigma import Enigma
 
-if __name__ == "__main__":
-    random.seed(100)
-    for i in range(5):
-        print(random.randint(1, 100))
+os.chdir(os.path.dirname(__file__))
+
+
+def encipher_string(seed: any, data: bytearray):
+    e = Enigma(seed)
+    encoded_buffer = bytearray()
+
+    for b in data:
+        encoded_byte = e.encipher(b)
+        encoded_buffer.append(encoded_byte)
+
+    return encoded_buffer
+
+
+def encipher_file(infile_path: str, outfile_path: str, seed: any):
+    # Open in and out files in binary format
+    with open(infile_path, "rb") as infd, open(outfile_path, "wb") as outfd:
+        #   Infinite loop we break out of when no more dtat is read
+        while True:
+            #         read in 64 bytes of data. This is usually a lot more
+            buffer = infd.read(64)
+            #            If we are not at the end of the file
+            if buffer:
+                #         Create bytearray to store data
+                enciphered_buffer = bytearray()
+                #     encipher each value
+                for b in buffer:
+                    encoded_byte = e.encipher(b)
+                    enciphered_buffer.append(encoded_byte)
+                outfd.write(enciphered_buffer)
+            else:
+                break
+
+    def main():
+        s = "your mom"
+        data = bytearray(s.encode("UTF-8"))
+        ciphertext = encipher_string(100, data)
+        print(ciphertext)
+
+        plaintext = encipher_string(100, ciphertext)
+        print(plaintext)
+
+    if __name__ == "__main__":
+        main()
